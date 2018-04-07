0d74d7a5d0e1a301d0771ffe7602d81cc01467b9
Prithvi Singh <prithvi.chandla@gmail.com>
2018-04-07 19:02:09 +0530

    Added more options in particle.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 var particles = Particles.init({
 	selector: '.background',
   color: ['#DA0463', '#404B69', '#DBEDF3'],
-  connectParticles: true
+  maxParticles : 100,
+  connectParticles: true,
+
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        maxParticles: 50,
+        color: '#48F2E3',
+        connectParticles: false
+      }
+    }, {
+      breakpoint: 425,
+      options: {
+        maxParticles: 25,
+        connectParticles: true
+      }
+    }, {
+      breakpoint: 320,
+      options: {
+        maxParticles: 0 // disables particles.js
+      }
+    }
+  ],
+
 });
