96413621c7c72e6be36dd84026919a08d80d524e
Prithvi Singh <prithvi.chandla@gmail.com>
2018-04-07 19:15:21 +0530

    Added

--- js/index.js
+++ js/index.js
@@ -1,29 +1,9 @@
+console.log(window.innerWidth);
+
 var particles = Particles.init({
 	selector: '.background',
   color: ['#DA0463', '#404B69', '#DBEDF3'],
-  maxParticles : 100,
   connectParticles: true,
-
-  responsive: [
-    {
-      breakpoint: 768,
-      options: {
-        maxParticles: 50,
-        color: '#48F2E3',
-        connectParticles: false
-      }
-    }, {
-      breakpoint: 425,
-      options: {
-        maxParticles: 25,
-        connectParticles: true
-      }
-    }, {
-      breakpoint: 320,
-      options: {
-        maxParticles: 0 // disables particles.js
-      }
-    }
-  ],
+  retina_detect: true
 
 });
